ab979fe44bad72808c2a12fe8e7c2984fb12a343
Gary Voo <leechunvoo@gmail.com>
2023-03-30 16:15:35 +0800

    Add project files.

---
+++ ReactTesting/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace ReactTesting.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; } = string.Empty;
+    }
+}
---
+++ ReactTesting/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace ReactTesting.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public string DateOfJoining { get; set; } = string.Empty;
+        public string PhotoFileName { get; set; } = string.Empty;
+
+    }
+}
